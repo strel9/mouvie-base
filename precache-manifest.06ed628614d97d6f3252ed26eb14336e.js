d8b4fcb13824e4c3b0e321cad7c28e154473126b
strel9 <strel9@gmail.com>
2020-05-31 20:06:16 +0300

    Updates

---
+++ precache-manifest.06ed628614d97d6f3252ed26eb14336e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596b3d7cbdf359695082e0f6df09d4c2",
+    "url": "/movie-base/index.html"
+  },
+  {
+    "revision": "f2eef36ba6461ac8da8d",
+    "url": "/movie-base/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "5c818699165f1eb291a9",
+    "url": "/movie-base/static/css/main.39812f68.chunk.css"
+  },
+  {
+    "revision": "f2eef36ba6461ac8da8d",
+    "url": "/movie-base/static/js/2.3e248b30.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movie-base/static/js/2.3e248b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c818699165f1eb291a9",
+    "url": "/movie-base/static/js/main.7b1e7d70.chunk.js"
+  },
+  {
+    "revision": "bd42c7eed8263302534b",
+    "url": "/movie-base/static/js/runtime-main.0ec1a4e0.js"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/movie-base/static/media/logo.02a9430b.svg"
+  },
+  {
+    "revision": "fcfc35e0ff2ae7aa2756c35786282738",
+    "url": "/movie-base/static/media/no_foto.fcfc35e0.jpg"
+  }
+]);
