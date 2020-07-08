b0d62d7d8bedd36e9a99b52f43826263c02e0498
strel9 <strel9@gmail.com>
2020-07-08 10:28:10 +0300

    movie page

--- src/Components/MoviePage/MoviePage.jsx
+++ src/Components/MoviePage/MoviePage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function MoviePage() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<div className="movie-page-main">
+			<div className="movie-page-main__img">
+				<img></img>
+			</div>
+			<div className="movie-page-main-content"></div>
+		</div>
+	);
 }
 
-export default MoviePage
+export default MoviePage;
